ae58d32c4150dbbf5874ff58bd8d42448da8567b
Silas Owu <owusilasyp00@gmail.com>
2024-11-06 09:06:28 -0800

    chore: add base routing configuration for app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { adminRoutes } from './views/admin/admin.route';
+import { trainerRoutes } from './views/trainer/trainer.route';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  ...adminRoutes,
+  ...trainerRoutes,
+  //general
+  {
+    path:'settings',
+    loadComponent: () => import('./views/settings/settings.component')
+    .then(m => m.SettingsComponent)
+  },
+  {
+    path:'help',
+    loadComponent: () => import('./views/help/help.component')
+    .then(m => m.HelpComponent)
+  }
+];
