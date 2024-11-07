0d567247c05696f731314a659c37335cc13b0329
Silas Owu <owusilasyp00@gmail.com>
2024-11-07 03:49:08 -0800

    style: update dashboard UI

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from "../../features/sidebar/sidebar.component";
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "../../features/header/header.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [SidebarComponent,RouterOutlet],
+  imports: [SidebarComponent, RouterOutlet, HeaderComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
