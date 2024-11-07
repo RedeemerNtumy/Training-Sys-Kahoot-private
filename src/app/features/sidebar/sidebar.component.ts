05889825acbadfbdb94f2e90029416b8b4e82894
Silas Owu <owusilasyp00@gmail.com>
2024-11-06 20:04:18 -0800

    chore: remove unwanted code

--- src/app/features/sidebar/sidebar.component.ts
+++ src/app/features/sidebar/sidebar.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { NavigationsComponent } from "./navigations/navigations.component";
+
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [NavigationsComponent],
+  imports: [],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
