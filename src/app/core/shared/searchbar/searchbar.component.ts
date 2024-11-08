eac2d1fa3675a5afa4cd79585b51a22a61ca70f9
Kojoel <nanaabruquah1s@gmail.com>
2024-11-08 09:12:19 +0000

    feat: make component resuable using Input diectives

--- src/app/core/shared/searchbar/searchbar.component.ts
+++ src/app/core/shared/searchbar/searchbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-searchbar',
@@ -8,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './searchbar.component.scss'
 })
 export class SearchbarComponent {
+  
+  @Input() title: string = 'Cohort';
+  @Input() placeholder: string = 'Name';
+  @Input() buttonContent: string = 'Add Cohort';
+
+  constructor() {}
 
 }
