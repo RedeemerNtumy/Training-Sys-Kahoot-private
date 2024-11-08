149021cc673b631bb3c8ca4ce8b2fde0c0c3f9c3
Kojoel <nanaabruquah1s@gmail.com>
2024-11-08 10:10:44 +0000

    render no-cohort notice if observableis empty else render the list of cohorts

--- src/app/features/cohorts/list-cohorts/list-cohorts.component.ts
+++ src/app/features/cohorts/list-cohorts/list-cohorts.component.ts
@@ -2,19 +2,19 @@ import { Component } from '@angular/core';
 import { CohortDataService } from '../../../core/services/cohort-data/cohort-data.service';
 import { Cohort, CohortList } from '../../../core/models/cohort.interface';
 import { Observable } from 'rxjs';
-import { AsyncPipe, NgFor, } from '@angular/common';
+import { AsyncPipe, NgFor, NgIf, } from '@angular/common';
 import { SearchbarComponent } from '../../../core/shared/searchbar/searchbar.component';
 
 @Component({
   selector: 'app-list-cohorts',
   standalone: true,
-  imports: [AsyncPipe, NgFor, SearchbarComponent],
+  imports: [AsyncPipe, NgFor, NgIf, SearchbarComponent],
   templateUrl: './list-cohorts.component.html',
   styleUrl: './list-cohorts.component.scss'
 })
 export class ListCohortsComponent {
   
-  cohortsList$!: Observable<CohortList[]>;
+  cohortsList$!: Observable<CohortList[]> | null;
 
   constructor(
     private cohortDataService: CohortDataService, 
