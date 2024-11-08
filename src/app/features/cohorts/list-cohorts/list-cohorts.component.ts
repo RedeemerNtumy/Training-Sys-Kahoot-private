c865f3570aea5bacc691cde315941eb23612d9c3
Kojoel <nanaabruquah1s@gmail.com>
2024-11-08 03:31:02 +0000

    feat: create and style list cohorts component

---
+++ src/app/features/cohorts/list-cohorts/list-cohorts.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-cohorts',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-cohorts.component.html',
+  styleUrl: './list-cohorts.component.scss'
+})
+export class ListCohortsComponent {
+
+}
