39b6819b42cce016639f7ed0a2169721e1e26f6f
Kojoel <nanaabruquah1s@gmail.com>
2024-11-09 14:54:07 +0000

    feat: edit cohort component created

---
+++ src/app/features/cohorts/edit-cohort/edit-cohort/edit-cohort.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-cohort',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-cohort.component.html',
+  styleUrl: './edit-cohort.component.scss'
+})
+export class EditCohortComponent {
+
+}
