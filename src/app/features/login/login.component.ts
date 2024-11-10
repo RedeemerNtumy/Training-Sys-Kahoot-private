6605f3b87d5297dc539b850ecb0f983e2993c21b
Eric-Obeng <obengeric.3958@gmail.com>
2024-11-10 20:03:10 +0000

    feat: add login component with form validation and styling

---
+++ src/app/features/login/login.component.ts
@@ -0,0 +1,55 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MessageComponent } from '../../core/shared/message/message.component';
+import { InputFieldComponent } from '../../core/shared/input-field/input-field.component';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    MessageComponent,
+    InputFieldComponent,
+    RouterModule,
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  showPasswordError = true;
+  showEmailError = false;
+  successMessage = '';
+  errorMessage = '';
+  isLoading = false;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initLoginForm();
+  }
+
+  initLoginForm() {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  onSubmit() {
+    this.isLoading = true;
+
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      const user = { email, password };
+    }
+  }
+}
