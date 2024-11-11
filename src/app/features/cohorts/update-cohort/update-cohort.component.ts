e6fcbe79497ed1ac826e3a38867eb60da5b3c814
Kojoel <nanaabruquah1s@gmail.com>
2024-11-11 22:48:40 +0000

    feat: created component for displaying data to be updated

---
+++ src/app/features/cohorts/update-cohort/update-cohort.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-cohort',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-cohort.component.html',
+  styleUrl: './update-cohort.component.scss'
+})
+export class UpdateCohortComponent {
+
+}
