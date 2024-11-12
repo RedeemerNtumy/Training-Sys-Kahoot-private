0251464445f454ca1eba7023a2c24aafb86e771e
Silas Owu <owusilasyp00@gmail.com>
2024-11-11 23:08:05 -0800

    feat: add delete functionality with confirmation modal to specialization list

---
+++ src/app/features/admin/specializations/delete-feedback/delete-feedback.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule ,} from '@angular/common';
+import { Component,EventEmitter, Input, Output } from '@angular/core';
+import { DialogModule } from 'primeng/dialog';
+// import { Component,  } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-feedback',
+  standalone: true,
+  imports: [CommonModule, DialogModule],
+  templateUrl: './delete-feedback.component.html',
+  styleUrl: './delete-feedback.component.scss'
+})
+
+export class DeleteFeedbackComponent {
+  @Input() visible = false;
+  @Output() visibleChange = new EventEmitter<boolean>();
+
+  close() {
+    this.visible = false;
+    this.visibleChange.emit(false);
+  }
+}
