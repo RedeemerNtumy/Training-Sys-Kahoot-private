bb3bf36056c07735ae8cec3e6cb362a2718c0507
Silas Owu <owusilasyp00@gmail.com>
2024-11-11 22:49:58 -0800

    feat: integrate JSON server for handling specialization data through service

--- src/app/core/models/specialization.interface.ts
+++ src/app/core/models/specialization.interface.ts
@@ -1,5 +1,8 @@
-export interface Ispecialization{
+export interface Ispecialization {
+  id: number;
   name: string;
   description: string;
   prerequisites: string[];
+  dateCreated?: string;
+  traineesCount?: number;
 }
--- src/app/core/services/specialization-facade/specialization-facade.service.ts
+++ src/app/core/services/specialization-facade/specialization-facade.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, throwError } from 'rxjs';
+import { BehaviorSubject, catchError, Observable, throwError,tap } from 'rxjs';
 import { Ispecialization } from '../../models/specialization.interface';
 
 @Injectable({
@@ -8,15 +8,62 @@ import { Ispecialization } from '../../models/specialization.interface';
 })
 
 export class SpecializationFacadeService {
+  specializationSubject = new BehaviorSubject<Ispecialization[]>([]);
+  specialization$ = this.specializationSubject.asObservable();
+
+  private localServer: string = 'http://localhost:3000/specializations';
   private createEndpoint: string = 'http://localhost:8089/api/specializations';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.loadSpecializations();
+  }
 
-  create(specialization: Ispecialization) {
-    return this.http.post(this.createEndpoint, specialization).pipe(
-      catchError(this.handleError)
+  private loadSpecializations(){
+    this.getAllSpecializations()
+      .subscribe({
+        next: (specializations)=> this.specializationSubject.next(specializations),
+        error: (error) => console.error('Error occurred while fetching specializations:', error)
+      })
+  }
+
+  getAllSpecializations():Observable<Ispecialization[]>{
+    return this.http.get<Ispecialization[]>(this.localServer)
+    .pipe(catchError(this.handleError));
+  }
+
+  getSpecializationById(id: number):Observable<Ispecialization>{
+    return this.http.get<Ispecialization>(`${this.localServer}/${id}`).pipe(
+      catchError(this.handleError),
+      tap(() => this.loadSpecializations())
     );
   }
+
+  create(specialization: Ispecialization) {
+    console.log('from service: creation done');
+    return this.http.post(this.localServer, specialization)
+    .pipe(
+      catchError(this.handleError),
+      tap(() => this.loadSpecializations())
+    );
+  }
+
+  update(id: number,specialization: Partial<Ispecialization>){
+    return this.http.patch<Ispecialization>(`${this.localServer}/${id}`,specialization)
+    .pipe(
+      catchError(this.handleError),
+      tap(() => this.loadSpecializations())
+    )
+  }
+
+  delete(id:number):Observable<void>{
+    console.log('from service: delete done');
+    return this.http.delete<void>(`${this.localServer}/${id}`)
+    .pipe(
+      catchError(this.handleError),
+      tap(() => this.loadSpecializations())
+    )
+  }
+
 
   private handleError(error: HttpErrorResponse) {
     if (error.status === 0) {
