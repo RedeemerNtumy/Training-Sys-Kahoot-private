b1948c6c961d6cb953d51b8644f30cd6b4a9d43e
Silas Owu <owusilasyp00@gmail.com>
2024-11-11 23:16:26 -0800

    feat: integrate specialization list with JSON server data and pagination

--- src/app/views/admin/specializations/specialization-management.component.ts
+++ src/app/views/admin/specializations/specialization-management.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-// import { NoSpecializationAddedComponent } from "../../../features/specializations/no-specialization-added/no-specialization-added.component";
 import { RouterOutlet } from '@angular/router';
-import { CreateSpecializationComponent } from '../../../features/admin/specializations/create/create-specialization.component';
-import { SpecializationListComponent } from "../../../features/admin/specializations/list/specialization-list.component";
-// import { CreateSpecializationComponent } from "../../../features/specializations/create/create-specialization.component";
+
 
 @Component({
   selector: 'app-specialization-management',
   standalone: true,
-  imports: [CreateSpecializationComponent, SpecializationListComponent],
+  imports: [RouterOutlet],
   templateUrl: './specialization-management.component.html',
   styleUrl: './specialization-management.component.scss'
 })
