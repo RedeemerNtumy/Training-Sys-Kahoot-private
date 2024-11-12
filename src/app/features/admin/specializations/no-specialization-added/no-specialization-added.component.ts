19836a98048d9d0017627ce8ae4c5b64c1acfb2c
Silas Owu <owusilasyp00@gmail.com>
2024-11-11 23:31:27 -0800

    chore: add route to creating specialization form

--- src/app/features/admin/specializations/no-specialization-added/no-specialization-added.component.ts
+++ src/app/features/admin/specializations/no-specialization-added/no-specialization-added.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-no-specialization-added',
   standalone: true,
+  imports:[RouterLink,MatButtonModule],
   templateUrl: './no-specialization-added.component.html',
   styleUrls: ['./no-specialization-added.component.scss']
 })
 export class NoSpecializationAddedComponent {
   constructor(private router: Router) {}
 
-  navigatetoCreate() {
-    console.log('routed to create specializations');
+  navigateToCreate() {
     this.router.navigate(['home', 'admin', 'specialization', 'create']);
   }
 }
