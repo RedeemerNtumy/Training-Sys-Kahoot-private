08a6418f6f4efa67e5658c6af8c54385c80cd081
Eric-Obeng <obengeric.3958@gmail.com>
2024-11-13 15:50:51 +0000

    chore: update routing configuration to include auth routes and landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authRoutes } from './core/routes/auth.routes';
 // import { DashboardComponent } from './views/admin/dashboard/dashboard.component';
 // import { TraineeManagementComponent } from './views/admin/trainees/trainee-management.component';
 import { adminRoutes } from './views/admin/admin.routes';
@@ -6,20 +7,32 @@ import { trainerRoutes } from './core/routes/trainer.routes';
 
 export const routes: Routes = [
   {
-    path:'home',
-    loadComponent: () => import('./views/dashboard/dashboard.component')
-    .then(m => m.DashboardComponent),
+    path: '',
+    loadComponent: () =>
+      import('./views/landing-page/landing-page.component').then(
+        (l) => l.LandingPageComponent
+      ),
+  },
+  ...authRoutes,
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./views/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
+      ),
     children: [
       {
         path: 'admin',
-        children: adminRoutes
+        children: adminRoutes,
       },
       {
-        path:'trainer',
-        loadComponent:() => import('./views/trainer/trainer.component')
-        .then(m => m.TrainerComponent),
-        children: trainerRoutes
+        path: 'trainer',
+        loadComponent: () =>
+          import('./views/trainer/trainer.component').then(
+            (m) => m.TrainerComponent
+          ),
+        children: trainerRoutes,
       },
     ],
-  }
+  },
 ];
