8f6a9edfc7198d7380a84abe09c76f6f92ef1f1d
Silas Owu <owusilasyp00@gmail.com>
2024-11-13 07:52:19 +0000

    chore: add base route configuration for trainee dashboard

---
+++ src/app/features/sidebar/trainee-navigations/trainee-navigations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trainee-navigations',
+  standalone: true,
+  imports: [],
+  templateUrl: './trainee-navigations.component.html',
+  styleUrl: './trainee-navigations.component.scss'
+})
+export class TraineeNavigationsComponent {
+
+}
