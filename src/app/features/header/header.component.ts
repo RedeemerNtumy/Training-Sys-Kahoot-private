1591eccda5a67ff054dcfc2f0710e9c1d7a38f1a
Kojoel <nanaabruquah1s@gmail.com>
2024-11-13 11:34:30 +0000

    feat: removed unwanted code

--- src/app/features/header/header.component.ts
+++ src/app/features/header/header.component.ts
@@ -24,7 +24,6 @@ export class HeaderComponent implements OnInit {
     private router: Router, 
     private userRoleService: UserRoleService,
     private buttonStateService: ButtonStateService,
-    private cdRef: ChangeDetectorRef
   ) {}
 
   public toggleDropDownBtn() {
