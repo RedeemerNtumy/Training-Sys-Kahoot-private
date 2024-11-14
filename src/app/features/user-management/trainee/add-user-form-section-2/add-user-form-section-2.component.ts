e66d136c4d6fa4b3df0922076e5e83a0c496da83
Kojoel <nanaabruquah1s@gmail.com>
2024-11-14 07:35:02 +0000

    feat: create second section of user form

---
+++ src/app/features/user-management/trainee/add-user-form-section-2/add-user-form-section-2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-user-form-section-2',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-user-form-section-2.component.html',
+  styleUrl: './add-user-form-section-2.component.scss'
+})
+export class AddUserFormSection2Component {
+
+}
