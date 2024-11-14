ff99da0ffb2aba902ccef03bf8917a4817ec9dbf
Kojoel <nanaabruquah1s@gmail.com>
2024-11-14 01:42:15 +0000

    feat: add user form for trainee created

---
+++ src/app/features/user-management/trainee/add-user-form/add-user-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-user-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-user-form.component.html',
+  styleUrl: './add-user-form.component.scss'
+})
+export class AddUserFormComponent {
+
+}
