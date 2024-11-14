a496ff7d227da01e4447613262216dff58faa939
Kojoel <nanaabruquah1s@gmail.com>
2024-11-14 11:47:27 +0000

    feat: rendering data for specialization and cohort from arrays

--- src/app/features/user-management/trainee/add-user-form-section-2/add-user-form-section-2.component.ts
+++ src/app/features/user-management/trainee/add-user-form-section-2/add-user-form-section-2.component.ts
@@ -1,17 +1,34 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-user-form-section-2',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgFor, FormsModule, NgIf],
   templateUrl: './add-user-form-section-2.component.html',
   styleUrl: './add-user-form-section-2.component.scss'
 })
 export class AddUserFormSection2Component {
 
   newUserFormSecTwo!: FormGroup;
+
+  allSpecializations = [
+    { label: 'UI/UX', value: 'UI/UX' },
+    { label: 'Frontend Engineering', value: 'Frontend' },
+    { label: 'Backend Engineering', value: 'Backend' }
+  ];
+
+  allCohorts = [
+    { value: 'Cohort 2.0' },
+    { value: 'Cohort 3.0' },
+    { value: 'Cohort 4.0' },
+    { value: 'Cohort 5.0' },
+    { value: 'Cohort 6.0' },
+    { value: 'Cohort 7.0' },
+    { value: 'Cohort 8.0' },
+  ];
 
   constructor(
     private fb: FormBuilder,
@@ -20,12 +37,22 @@ export class AddUserFormSection2Component {
 
   ngOnInit() {
     this.newUserFormSecTwo = this.fb.group({
+      specialization: ['', Validators.required],
+      cohort: this.fb.array([this.fb.control('', Validators.required)]),
       enrollmentDate: ['', Validators.required],
       trainingID: ['', Validators.required]
     })
   }
 
 
+  // Get specializations and cohort for from form
+  get specialization(): FormArray {
+    return this.newUserFormSecTwo.get('specialization') as FormArray;
+  }
+  get cohort(): FormArray {
+    return this.newUserFormSecTwo.get('cohort') as FormArray;
+  }
+  
   goBack() {
     this.router.navigate(['/home/admin/user-managment/add-user'])
   }
