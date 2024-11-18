75dfb246c1b48cae5d82e7d061c8808213a04030
Eric-Obeng <obengeric.3958@gmail.com>
2024-11-18 09:32:11 +0000

    chore: integrate authentication routes into main routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-// import { DashboardComponent } from './views/admin/dashboard/dashboard.component';
-// import { TraineeManagementComponent } from './views/admin/trainees/trainee-management.component';
+import { authRoutes } from './core/routes/auth.routes';
 import { adminRoutes } from './views/admin/admin.routes';
 import { trainerRoutes } from './core/routes/trainer.routes';
 
 export const routes: Routes = [
+  ...authRoutes,
   {
     path:'home',
     loadComponent: () => import('./views/dashboard/dashboard.component')
