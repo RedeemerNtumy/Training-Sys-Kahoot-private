f25e28f0e32906dde9c961c3f2e922c7e8229ccd
Eric-Obeng <obengeric.3958@gmail.com>
2024-11-18 13:36:05 +0000

    feat: refactor FAQ component to load data from FaqsService and improve template structure

--- src/app/views/landing-page/components/faq/faq.component.ts
+++ src/app/views/landing-page/components/faq/faq.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AccordionModule } from 'primeng/accordion';
+import { Faq, FaqsService } from '../../../../core/services/faq/faqs.service';
 
 @Component({
   selector: 'app-faq',
@@ -10,42 +11,11 @@ import { AccordionModule } from 'primeng/accordion';
   styleUrl: './faq.component.scss',
 })
 export class FAQComponent {
-  faqs = [
-    {
-      question: 'What is Training Suite?',
-      answer:
-        'Training Suite is a platform that allows you to train and improve your skills. It is designed to help you get started with your training journey.',
-    },
-    {
-      question: 'How does Training Suite help me?',
-      answer:
-        'Training Suite is a platform that allows you to train and improve your skills. It is designed to help you get started with your training journey.',
-    },
-    {
-      question: 'Can I track trainee progress in real-time?',
-      answer:
-        'Training Suite is a platform that allows you to train and improve your skills. It is designed to help you get started with your training journey.',
-    },
-    {
-      question: 'How are quizzes and assignments graded?',
-      answer:
-        'Training Suite is a platform that allows you to train and improve your skills. It is designed to help you get started with your training journey.',
-    },
-    {
-      question: 'Can trainees access their grades and feedback?',
-      answer:
-        'Training Suite is a platform that allows you to train and improve your skills. It is designed to help you get started with your training journey.',
-    },
-    {
-      question: 'What security measures are in place?',
-      answer:
-        'Training Suite is a platform that allows you to train and improve your skills. It is designed to help you get started with your training journey.',
-    },
-    {
-      question:
-        "Is it possible to update curriculum materials after they've been created?",
-      answer:
-        'Training Suite is a platform that allows you to train and improve your skills. It is designed to help you get started with your training journey.',
-    },
-  ];
+  faqs: Faq[] = [];
+
+  constructor(private faqService: FaqsService) {
+    this.faqService.getFaqs().subscribe((data: Faq[]) => {
+      this.faqs = data;
+    });
+  }
 }
