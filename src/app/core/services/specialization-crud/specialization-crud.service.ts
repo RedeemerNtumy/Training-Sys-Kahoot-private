936018095aa2442385f303740c77662cc40c93f9
Silas Owu <owusilasyp00@gmail.com>
2024-11-19 18:36:34 +0000

    chore: update environment naming

--- src/app/core/services/specialization-crud/specialization-crud.service.ts
+++ src/app/core/services/specialization-crud/specialization-crud.service.ts
@@ -10,7 +10,7 @@ import { environment } from 'src/environments/environment';
 })
 
 export class SpecializationCrudService {
-  private hostedServer = environment.apiUrl;
+  private hostedServer = environment.BaseUrl;
 
   constructor(
     private http: HttpClient,
