f2195492b64d44f04c70cf45737ec5636e742b75
Eric-Obeng <obengeric.3958@gmail.com>
2024-11-21 08:18:03 +0000

    feat: enhance add-user component to include trainer data and improve filtering functionality

--- src/app/features/user-management/trainee/add-user/add-user.component.ts
+++ src/app/features/user-management/trainee/add-user/add-user.component.ts
@@ -3,27 +3,38 @@ import { Router, RouterModule } from '@angular/router';
 import { SearchbarComponent } from '../../../../core/shared/searchbar/searchbar.component';
 import { AsyncPipe, NgFor, NgIf, TitleCasePipe } from '@angular/common';
 import { TraineeInsystemService } from '../../../../core/services/user-management/trainee/trainee-insystem.service';
-import { BehaviorSubject, Observable, catchError, combineLatest, map } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest, map } from 'rxjs';
 import { User } from '../../../../core/models/cohort.interface';
 import { TraineeListComponent } from './trainee-list/trainee-list.component';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatTabChangeEvent } from '@angular/material/tabs';
+import { TrainerListComponent } from '../../trainer/trainer-list/trainer-list.component';
+import { Trainer } from '@core/models/trainer.interface';
+import { TrainerService } from '@core/services/user-management/trainer/trainer.service';
 
 @Component({
   selector: 'app-add-user',
   standalone: true,
-  imports: [SearchbarComponent, RouterModule, AsyncPipe, NgIf, TraineeListComponent,MatTabsModule],
+  imports: [
+    SearchbarComponent,
+    RouterModule,
+    AsyncPipe,
+    NgIf,
+    TraineeListComponent,
+    MatTabsModule,
+    TrainerListComponent,
+  ],
   templateUrl: './add-user.component.html',
   styleUrl: './add-user.component.scss',
 })
 export class AddUserComponent {
-
   traineeUsers$!: Observable<User[]>;
+  trainersData$!: Observable<Trainer[]>;
   filteredTrainees$!: Observable<User[]>;
   trainerTabClicked: boolean = true;
   deleteTraineeEmail: string = '';
 
-  private searchTerm$ = new BehaviorSubject<string>(''); 
+  private searchTerm$ = new BehaviorSubject<string>('');
   private statusFilter$ = new BehaviorSubject<string | null>(null);
   private specializationFilter$ = new BehaviorSubject<string | null>(null);
 
@@ -32,52 +43,54 @@ export class AddUserComponent {
   isConfirmDeleteModalOpen = false;
 
   deleteModalSuccess = false;
-  
+
   constructor(
     private router: Router,
-    private traineesInsystemService: TraineeInsystemService
+    private traineesInsystemService: TraineeInsystemService,
+    private trainersService: TrainerService
   ) {}
-
-
 
   ngOnInit() {
     // Get cohort details with trainees list from service
-    this.traineeUsers$ = this.traineesInsystemService.getAllTrainees(); 
+    this.traineeUsers$ = this.traineesInsystemService.getAllTrainees();
+    this.trainersData$ = this.trainersService.getAllTrainers();
 
     this.filteredTrainees$ = combineLatest([
-      this.traineeUsers$, 
-      this.searchTerm$, 
-      this.statusFilter$, 
-      this.specializationFilter$
+      this.traineeUsers$,
+      this.searchTerm$,
+      this.statusFilter$,
+      this.specializationFilter$,
     ]).pipe(
       map(([trainees, searchTerm, statusFilter, specFilter]) => {
         const lowerSearchTerm = searchTerm.toLowerCase();
-    
+
         return trainees.filter((trainee: User) => {
           // Check if trainee matches the search term
-          const matchesSearch = 
-            trainee.firstName.toLowerCase().includes(lowerSearchTerm) || 
-            trainee.lastName.toLowerCase().includes(lowerSearchTerm) || 
-            trainee.email.toLowerCase().includes(lowerSearchTerm) || 
+          const matchesSearch =
+            trainee.firstName.toLowerCase().includes(lowerSearchTerm) ||
+            trainee.lastName.toLowerCase().includes(lowerSearchTerm) ||
+            trainee.email.toLowerCase().includes(lowerSearchTerm) ||
             trainee.phoneNumber.includes(lowerSearchTerm);
-    
+
           // Check if trainee matches the status filter
-          const matchesStatus = statusFilter ? trainee.status === statusFilter : true;
-    
+          const matchesStatus = statusFilter
+            ? trainee.status === statusFilter
+            : true;
+
           // Check if trainee matches the specialization filter
-          const matchesSpecialization = specFilter ? trainee.specialization === specFilter : true;
-    
+          const matchesSpecialization = specFilter
+            ? trainee.specialization === specFilter
+            : true;
+
           return matchesSearch && matchesStatus && matchesSpecialization;
         });
       })
     );
-    
   }
 
   tabClicked() {
     this.trainerTabClicked = !this.trainerTabClicked;
   }
-
 
   // Update search term on changes from the search bar
   onSearchChange(searchTerm: string): void {
@@ -86,19 +99,21 @@ export class AddUserComponent {
 
   onSortList() {
     this.filteredTrainees$ = this.filteredTrainees$.pipe(
-      map((trainees: User[]) => trainees.sort((a, b) => a.firstName.localeCompare(b.firstName)))
+      map((trainees: User[]) =>
+        trainees.sort((a, b) => a.firstName.localeCompare(b.firstName))
+      )
     );
   }
-  
+
   // Set the filter for each status and trigger re-evaluation
   filterByActive() {
     this.statusFilter$.next('active');
   }
-  
+
   filterByInactive() {
     this.statusFilter$.next('inactive');
   }
-  
+
   filterByDeactivated() {
     this.statusFilter$.next('deactivated');
   }
@@ -108,20 +123,20 @@ export class AddUserComponent {
   }
 
   filterBySpecialization(spec: string) {
-    this.specializationFilter$.next(spec)
+    this.specializationFilter$.next(spec);
   }
 
   clearSpecializationFilter() {
     this.specializationFilter$.next(null);
   }
 
-  toggleEllipsis(selectedTrainee: string, event:Event) {
+  toggleEllipsis(selectedTrainee: string, event: Event) {
     event.stopPropagation();
-    this.selectedTraineeName = this.selectedTraineeName === selectedTrainee ? null : selectedTrainee;
-    if(this.selectedTraineeName === null) {
+    this.selectedTraineeName =
+      this.selectedTraineeName === selectedTrainee ? null : selectedTrainee;
+    if (this.selectedTraineeName === null) {
       this.ellipsisClicked = false;
-    }
-    else if(this.selectedTraineeName === selectedTrainee) {
+    } else if (this.selectedTraineeName === selectedTrainee) {
       this.ellipsisClicked = true;
     }
   }
@@ -132,7 +147,7 @@ export class AddUserComponent {
   }
 
   goToProfile(id: string) {
-    this.router.navigate(['/home/admin/user-management/user-profile/'])
+    this.router.navigate(['/home/admin/user-management/user-profile/']);
   }
 
   deleteUser(email: string) {
@@ -141,9 +156,9 @@ export class AddUserComponent {
   }
 
   confirmDelete() {
-    this.traineesInsystemService.deleteSelectedTrainee(this.deleteTraineeEmail)
+    this.traineesInsystemService.deleteSelectedTrainee(this.deleteTraineeEmail);
     this.toggleConfirmDeleteModal();
-    if(this.traineesInsystemService.deleteModalSuccessful) {
+    if (this.traineesInsystemService.deleteModalSuccessful) {
       this.toggleDeleteModalSuccess();
     }
   }
@@ -168,26 +183,15 @@ export class AddUserComponent {
     }
   }
 
-
   setToTrainerTab() {
     this.trainerTabClicked = true;
-    console.log(this.trainerTabClicked, 'trainer')
+    console.log(this.trainerTabClicked, 'trainer');
   }
 
   setToTraineeTab() {
     this.trainerTabClicked = false;
-    console.log(this.trainerTabClicked, 'trainee')
-
+    console.log(this.trainerTabClicked, 'trainee');
   }
-
-
-
-
-
-
-
-
-
 
   goToAddUserForm() {
     this.router.navigate(['/home/admin/user-management/add-user-form']);
