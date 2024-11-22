1eb9f9be054178448cf246e8a3bad62b891bb6f1
Kojoel <nanaabruquah1s@gmail.com>
2024-11-22 17:09:24 +0000

    feat: get assessments from assessment service for conditionally rendering assessment type

--- src/app/features/grade-management/view-assessment-list/view-assessment-list.component.ts
+++ src/app/features/grade-management/view-assessment-list/view-assessment-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { TabViewModule } from 'primeng/tabview';
 import { ViewAssignmentsComponent } from './view-assignments/view-assignments.component';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-view-assessment-list',
@@ -11,5 +12,37 @@ import { ViewAssignmentsComponent } from './view-assignments/view-assignments.co
 })
 export class ViewAssessmentListComponent {
 
+  getAssessments$!: Observable<any>;
+
+  UngradedCount: number = 0;
+  GradedCount: number = 0;
+  hasUngraded: boolean = false;
+  hasGraded: boolean = false;
+
+  constructor() {
+    private assessmentService: assessmentService,
+  }
+
+  ngOnInit() {
+    // Call function to get assessments
+    this.init();
+  }
+
+  init() {
+    // Return all the assessments (graded and ungraded)
+    this.getAssessments$ = this.assessmentService.getAssessments();
+
+    // Assign graded and ungraded lengths to different variables
+    this.getAssessments$.subscribe(assessments => {
+      // Calculate lengths
+      this.UngradedCount = data.Ungraded.length;
+      this.GradedCount = data.Graded.length;
+
+      // Set boolean values
+      this.hasUngraded = this.UngradedCount > 0;
+      this.hasGraded = this.GradedCount > 0;
+    }) 
+
+  }
 
 }
