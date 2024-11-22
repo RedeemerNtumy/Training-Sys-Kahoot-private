5d7657dd7e0fa2c77075d7440f472d8d5b46fe80
Kojoel <nanaabruquah1s@gmail.com>
2024-11-22 10:21:49 +0000

    feat: add Assessment and grade history tabs from primeng

--- src/app/features/grade-management/view-assessment-list/view-assessment-list.component.ts
+++ src/app/features/grade-management/view-assessment-list/view-assessment-list.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { TabMenuModule } from 'primeng/tabmenu';
 
 @Component({
   selector: 'app-view-assessment-list',
   standalone: true,
-  imports: [],
+  imports: [TabMenuModule],
   templateUrl: './view-assessment-list.component.html',
   styleUrl: './view-assessment-list.component.scss'
 })
 export class ViewAssessmentListComponent {
+
+  
+  items = [
+    {label: "Assessment"}, 
+    {label: "Grade History"}
+  ];
+
+  activeItem = 0;
 
 }
