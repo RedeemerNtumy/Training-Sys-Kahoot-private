f19af618847e6443ca39d83cfa1373211b1f095a
Kojoel <nanaabruquah1s@gmail.com>
2024-11-22 10:30:25 +0000

    feat: create view-assignments component for showing list of assignments

--- src/app/features/grade-management/view-assessment-list/view-assessment-list.component.ts
+++ src/app/features/grade-management/view-assessment-list/view-assessment-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TabMenuModule } from 'primeng/tabmenu';
+import { ViewAssignmentsComponent } from './view-assignments/view-assignments.component';
 
 @Component({
   selector: 'app-view-assessment-list',
   standalone: true,
-  imports: [TabMenuModule],
+  imports: [TabMenuModule, ViewAssignmentsComponent],
   templateUrl: './view-assessment-list.component.html',
   styleUrl: './view-assessment-list.component.scss'
 })
---
+++ src/app/features/grade-management/view-assessment-list/view-assignments/view-assignments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-assignments',
+  standalone: true,
+  imports: [],
+  templateUrl: './view-assignments.component.html',
+  styleUrl: './view-assignments.component.scss'
+})
+export class ViewAssignmentsComponent {
+
+}
