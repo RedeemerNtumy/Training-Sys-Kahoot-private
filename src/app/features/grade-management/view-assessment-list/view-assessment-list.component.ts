5a16cd897ea6ea8fefc8bdde30f772b74d4a4080
Kojoel <nanaabruquah1s@gmail.com>
2024-11-23 16:09:28 +0000

    style: apply styles to the view grade history component

--- src/app/features/grade-management/view-assessment-list/view-assessment-list.component.ts
+++ src/app/features/grade-management/view-assessment-list/view-assessment-list.component.ts
@@ -3,11 +3,12 @@ import { TabViewModule } from 'primeng/tabview';
 import { ViewAssignmentsComponent } from './view-assignments/view-assignments.component';
 import { Observable, of } from 'rxjs';
 import { NgIf } from '@angular/common';
+import { ViewGradeHistoryComponent } from './view-grade-history/view-grade-history.component';
 
 @Component({
   selector: 'app-view-assessment-list',
   standalone: true,
-  imports: [TabViewModule, ViewAssignmentsComponent, NgIf],
+  imports: [TabViewModule, ViewAssignmentsComponent, NgIf, ViewGradeHistoryComponent],
   templateUrl: './view-assessment-list.component.html',
   styleUrl: './view-assessment-list.component.scss'
 })
--- src/app/features/grade-management/view-assessment-list/view-grade-history/view-grade-history.component.ts
+++ src/app/features/grade-management/view-assessment-list/view-grade-history/view-grade-history.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchbarComponent } from '@core/shared/searchbar/searchbar.component';
 
 @Component({
   selector: 'app-view-grade-history',
   standalone: true,
-  imports: [],
+  imports: [SearchbarComponent],
   templateUrl: './view-grade-history.component.html',
   styleUrl: './view-grade-history.component.scss'
 })
