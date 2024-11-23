c15fb6e0ddc3d742702827767d950c18dcf90dc4
Eric-Obeng <obengeric.3958@gmail.com>
2024-11-23 16:39:37 +0000

    chore: implement TokenService for managing and decoding JWT tokens

---
+++ src/app/core/services/token/token.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { DecodedToken } from '@core/models/iuser';
+import { jwtDecode } from 'jwt-decode';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TokenService {
+  private tokenSubject = new BehaviorSubject<DecodedToken | null>(null);
+
+  constructor() {
+    const storedToken = localStorage.getItem('token');
+    if (storedToken) {
+      this.setToken(storedToken);
+    }
+  }
+
+  setToken(token: string) {
+    localStorage.setItem('token', token);
+    const decodedToken = jwtDecode<DecodedToken>(token);
+    this.tokenSubject.next(decodedToken);
+  }
+
+  getDecodedToken() {
+    return this.tokenSubject.asObservable();
+  }
+
+  getDecodedTokenValue() {
+    return this.tokenSubject.value;
+  }
+
+  clearToken() {
+    localStorage.removeItem('token');
+    this.tokenSubject.next(null);
+  }
+}
