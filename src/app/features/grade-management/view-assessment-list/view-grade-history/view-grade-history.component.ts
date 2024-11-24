2f0a0e5f3532689596f433f072ef9522c8ded592
Kojoel <nanaabruquah1s@gmail.com>
2024-11-24 16:34:41 +0000

    feat: add paginator to view grade history page

--- src/app/features/grade-management/view-assessment-list/view-grade-history/view-grade-history.component.ts
+++ src/app/features/grade-management/view-assessment-list/view-grade-history/view-grade-history.component.ts
@@ -1,10 +1,12 @@
 import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { PaginatorComponent } from '@core/shared/paginator/paginator.component';
 import { SearchbarComponent } from '@core/shared/searchbar/searchbar.component';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-view-grade-history',
   standalone: true,
-  imports: [SearchbarComponent],
+  imports: [SearchbarComponent, PaginatorComponent],
   templateUrl: './view-grade-history.component.html',
   styleUrl: './view-grade-history.component.scss'
 })
@@ -13,6 +15,12 @@ export class ViewGradeHistoryComponent {
   @ViewChild('ellipsisIcon') ellipsisIcon!: ElementRef;
 
   showEllipseOptions: boolean = false;
+
+  //Pagination 
+  private pageSubject = new BehaviorSubject<number>(1);
+  currentPage$ = this.pageSubject.asObservable();
+  pageSize = 4;
+  totalPages = 1;
 
   constructor() {}
 
@@ -27,6 +35,12 @@ export class ViewGradeHistoryComponent {
     if(this.ellipsisIcon && !this.ellipsisIcon.nativeElement.contains(event.target)) {
       this.showEllipseOptions = false;
     }
+  }
+
+
+  // Pagination 
+  onPageChange(page: number) {
+    this.pageSubject.next(page);
   }
  
 }
