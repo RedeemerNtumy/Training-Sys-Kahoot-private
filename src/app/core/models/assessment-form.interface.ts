c96c667333d825cf85eaebb82031d0bb3156d4cb
Eric-Obeng <obengeric.3958@gmail.com>
2024-11-28 10:08:24 +0000

    feat: extend AssessmentType and update AssessmentData interface for additional assessment types and properties

--- src/app/core/models/assessment-form.interface.ts
+++ src/app/core/models/assessment-form.interface.ts
@@ -1,10 +1,19 @@
-export type AssessmentType = 'quiz' | 'lab';
+export type AssessmentType = 'quiz' | 'lab' | 'presentation';
 
 export interface AssessmentData {
   assessmentType: AssessmentType;
   title: string;
   focusArea: string;
   description: string;
-  coverImage?: File;
+  coverImage: File;
   attachments?: File[];
+  questions?: any[];
+  questionsCount?: number;
+}
+
+export interface CreateAssessment {
+  type: AssessmentType;
+  label: string;
+  icon: string;
+  route: string;
 }
