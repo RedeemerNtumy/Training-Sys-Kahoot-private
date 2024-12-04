d43ac4a9c199eb9e1bcb4d2ee290b8b4e4e38ee2
Eric-Obeng <obengeric.3958@gmail.com>
2024-12-04 17:21:13 +0000

    refactor: enhance assessment service to load assessments on initialization and improve error handling

--- src/app/core/services/assessment/assessment.service.ts
+++ src/app/core/services/assessment/assessment.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, retry, tap, throwError, BehaviorSubject } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  catchError,
+  Observable,
+  retry,
+  tap,
+  throwError,
+  BehaviorSubject,
+} from 'rxjs';
 import {
   AssessmentData,
   AssessmentType,
   CreateAssessment,
+  Quiz,
 } from '@core/models/assessment-form.interface';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
@@ -16,7 +25,13 @@ export class AssessmentService {
   private assessmentsSubject = new BehaviorSubject<AssessmentData[]>([]);
   assessments$ = this.assessmentsSubject.asObservable();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadAssessments();
+  }
+
+  private loadAssessments() {
+    this.getAssessments().subscribe();
+  }
 
   getAssessmentType() {
     return this.http.get<CreateAssessment[]>(this.getAsessmentTypeUrl);
@@ -24,24 +39,45 @@ export class AssessmentService {
 
   // Get all assessments without filtering
   getAssessments(): Observable<AssessmentData[]> {
-    return this.http.get<AssessmentData[]>(this.apiUrl).pipe(
-      tap((data) => this.assessmentsSubject.next(data)),
-      retry(2),
-      catchError((error) => {
-        console.error('Error fetching assessments:', error);
-        return throwError(() => error);
+    const headers = new HttpHeaders({
+      'ngrok-skip-browser-warning': '45678',
+    });
+    return this.http
+      .get<AssessmentData[]>(`${environment.BaseUrl}/assessments/all`, {
+        headers,
       })
-    );
+      .pipe(
+        tap((data) => {
+          console.log('Assessments fetched successfully:', data);
+          this.assessmentsSubject.next(data);
+        }),
+        retry(2),
+        catchError((error) => {
+          console.error('Error fetching assessments:', error);
+          return throwError(() => error);
+        })
+      );
   }
 
   // Add a new assessment
-  addAssessment(assessment: AssessmentData): Observable<AssessmentData> {
-    return this.http.post<AssessmentData>(this.apiUrl, assessment).pipe(
-      retry(2),
-      catchError((error) => {
-        console.error('Error adding assessment:', error);
-        return throwError(() => error);
-      })
-    );
+  addAssessment(
+    assessment: Quiz,
+    timeFrame: number
+  ): Observable<AssessmentData> {
+    const quizId = JSON.parse(localStorage.getItem('quizId') || 'null');
+    if (!quizId) {
+      throw new Error('Quiz ID not found in local storage');
+    }
+    return this.http
+      .post<AssessmentData>(
+        `${environment.BaseUrl}/quizzes/${quizId}/questions/batch?quizDuration=${timeFrame}`,
+        assessment
+      )
+      .pipe(
+        catchError((error) => {
+          console.error('Error adding assessment:', error);
+          return throwError(() => error);
+        })
+      );
   }
 }
