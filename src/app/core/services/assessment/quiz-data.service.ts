c212d3cd881a9a3523582e62594ca63f7983230e
Eric-Obeng <obengeric.3958@gmail.com>
2024-12-04 17:21:06 +0000

    refactor: enhance quiz data service to support FormData submission and optional backend posting

--- src/app/core/services/assessment/quiz-data.service.ts
+++ src/app/core/services/assessment/quiz-data.service.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of, tap } from 'rxjs';
 import { AssessmentData } from '@core/models/assessment-form.interface';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class QuizDataService {
-  private quizDataSubject = new BehaviorSubject<AssessmentData | null>(this.loadFromSessionStorage());
+  private quizDataSubject = new BehaviorSubject<AssessmentData | null>(
+    this.loadFromSessionStorage()
+  );
 
-  setQuizData(data: AssessmentData) {
-    this.quizDataSubject.next(data);
-    sessionStorage.setItem('quizData', JSON.stringify(data));
+  constructor(private http: HttpClient) {}
+
+  setQuizData(data: FormData, postToBackend: boolean = false): Observable<AssessmentData | null> {
+    if (postToBackend) {
+      return this.http.post<AssessmentData>(`${environment.BaseUrl}/assessments/quiz`, data).pipe(
+        tap((response) => {
+          this.quizDataSubject.next(response);
+          sessionStorage.setItem('quizData', JSON.stringify(response));
+        })
+      );
+    } else {
+      this.quizDataSubject.next(data as unknown as AssessmentData);
+      sessionStorage.setItem('quizData', JSON.stringify(data));
+      return of(null);
+    }
   }
 
   getQuizData(): Observable<AssessmentData | null> {
