ed60c921c38b2259dea923ffb9ddacf4575c23c8
Eric-Obeng <obengeric.3958@gmail.com>
2024-12-05 08:20:29 +0000

    Add KahootHeaderComponent with template, styles, and test suite

---
+++ src/app/features/assessment-system/components/kahoot-header/kahoot-header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kahoot-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './kahoot-header.component.html',
+  styleUrl: './kahoot-header.component.scss'
+})
+export class KahootHeaderComponent {
+
+}
