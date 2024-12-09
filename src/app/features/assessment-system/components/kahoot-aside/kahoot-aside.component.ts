d903d77b15ff86a45abc64a71da1b67d897504a5
Eric-Obeng <obengeric.3958@gmail.com>
2024-12-09 08:01:36 +0000

    Implement dropdown selections for time limit, points, answer options, and code snippets in Kahoot aside component

--- src/app/features/assessment-system/components/kahoot-aside/kahoot-aside.component.ts
+++ src/app/features/assessment-system/components/kahoot-aside/kahoot-aside.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { initializeDropdownOptions } from '../../../../utils/kahootDropDownData';
 
 @Component({
   selector: 'app-kahoot-aside',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, DropdownModule, CommonModule],
   templateUrl: './kahoot-aside.component.html',
-  styleUrl: './kahoot-aside.component.scss'
+  styleUrl: './kahoot-aside.component.scss',
 })
-export class KahootAsideComponent {
+export class KahootAsideComponent implements OnInit {
+  timeLimit: { name: string }[] | undefined;
 
+  ngOnInit(): void {
+    const dropdownOptions = initializeDropdownOptions();
+    this.timeLimit = dropdownOptions.timeLimit;
+  }
 }
