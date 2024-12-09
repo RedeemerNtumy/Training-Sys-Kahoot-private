747c55533a8aed895627c1d7d90a8347b14068cc
Eric-Obeng <obengeric.3958@gmail.com>
2024-12-09 08:01:09 +0000

    Add Generate button and AI modal functionality to Kahoot header

--- src/app/features/assessment-system/components/kahoot-header/kahoot-header.component.ts
+++ src/app/features/assessment-system/components/kahoot-header/kahoot-header.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { GenerateByAiComponent } from '../../kahoot-page/generate-by-ai/generate-by-ai.component';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-kahoot-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, GenerateByAiComponent, RouterModule, RouterLink],
   templateUrl: './kahoot-header.component.html',
-  styleUrl: './kahoot-header.component.scss'
+  styleUrl: './kahoot-header.component.scss',
 })
 export class KahootHeaderComponent {
+  openAiModal: boolean = false;
 
+  onOpenAiModal() {
+    this.openAiModal = true;
+  }
+
+  onCloseAiModal() {
+    this.openAiModal = false;
+  }
 }
