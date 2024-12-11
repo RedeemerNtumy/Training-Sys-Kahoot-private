d62a7722f8caa73722de26952990aa8c0c31914d
Eric-Obeng <obengeric.3958@gmail.com>
2024-12-11 18:45:20 +0000

    feat: update trainee list component to display full names and handle trainee selection

--- src/app/features/assessment-system/components/trainee-list/trainee-list.component.ts
+++ src/app/features/assessment-system/components/trainee-list/trainee-list.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
 import { TraineeInsystemService } from '@core/services/user-management/trainee/trainee-insystem.service';
 import { User } from '@core/models/cohort.interface';
 import { catchError, filter, Observable, of, map } from 'rxjs';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-trainee-list',
@@ -12,12 +13,24 @@ import { catchError, filter, Observable, of, map } from 'rxjs';
   templateUrl: './trainee-list.component.html',
   styleUrl: './trainee-list.component.scss',
 })
-export class TraineeListComponent implements OnInit {
-  trainees$!: Observable<User[]>;
+export class TraineeListComponent {
+  @Input() trainees: User[] = [];
+  @Output() selectedTrainees = new EventEmitter<string[]>();
 
-  constructor(private traineeService: TraineeInsystemService) {}
+  @Input() selectAllChecked = false;
 
-  ngOnInit(): void {
-    this.trainees$ = this.traineeService.getAllTrainees();
+  selectedTraineeEmails: string[] = [];
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  onTraineeSelect(trainee: User, event: any) {
+    if (event.target.checked) {
+      this.selectedTraineeEmails.push(trainee.email);
+    } else {
+      this.selectedTraineeEmails = this.selectedTraineeEmails.filter(email => email !== trainee.email);
+    }
+    this.selectAllChecked = this.selectedTraineeEmails.length === this.trainees.length;
+    this.selectedTrainees.emit(this.selectedTraineeEmails);
   }
+
 }
