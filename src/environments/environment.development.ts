8f238caf7616f012a59dbdacf443212bfd56e14a
Eric-Obeng <obengeric.3958@gmail.com>
2024-12-11 09:32:26 +0000

    Update BaseUrl in development environment configuration

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  BaseUrl: 'https://api.training-management.amalitech-dev.net/api/v1',
+  BaseUrl: 'https://422f-196-61-35-158.ngrok-free.app/api/v1',
 };
