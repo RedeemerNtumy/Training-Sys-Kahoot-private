5dfed71752d5b670f556ee0f55a1c45023b41aed
Silas Owu <owusilasyp00@gmail.com>
2024-12-13 12:52:01 +0000

    refactor(SpecializationCrudService): improve specialization service to enhance headers handling

--- src/app/core/services/specialization-crud/specialization-crud.service.ts
+++ src/app/core/services/specialization-crud/specialization-crud.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {  catchError, Observable,tap } from 'rxjs';
 import { ErrorHandleService } from '../error-handle/error-handle.service';
 import { environment } from '../../../../environments/environment.development';
@@ -11,7 +11,12 @@ import { specialization } from '../../models/specialization.interface';
 })
 
 export class SpecializationCrudService {
-  private hostedServer = environment.BaseUrl;
+  // private hostedServer = environment.BaseUrl;
+  private hostedServer = 'https://75fc-196-61-35-158.ngrok-free.app/api/v1'
+  private headers = new HttpHeaders({
+    'ngrok-skip-browser-warning': '69420'
+  });
+
 
   constructor(
     private http: HttpClient,
@@ -19,7 +24,7 @@ export class SpecializationCrudService {
   ) { }
 
   getAllSpecializations():Observable<specialization[]>{
-    return this.http.get<specialization[]>(`${this.hostedServer}/specializations`)
+    return this.http.get<specialization[]>(`${this.hostedServer}/specializations`,{ headers: this.headers })
     .pipe(
       catchError(this.errorService.handleError)
     );
@@ -27,21 +32,21 @@ export class SpecializationCrudService {
 
   getSpecializationById(id: number):Observable<specialization>{
     const url = `${this.hostedServer}/specializations/${id}`;
-    return this.http.get<specialization>(url)
+    return this.http.get<specialization>(url, { headers: this.headers })
   }
 
   createSpecialization(specialization: specialization){
     const url = `${this.hostedServer}/specializations`
-    return this.http.post(url, specialization)
+    return this.http.post(url, specialization, { headers: this.headers })
   }
 
   updateSpecialization(id: number, specialization: Partial<specialization>):Observable<specialization>{
     const url = `${this.hostedServer}/specializations/${id}`;
-    return this.http.put<specialization>(url, specialization)
+    return this.http.put<specialization>(url, specialization, { headers: this.headers })
   }
 
   deleteSpecialization(id: number):Observable<void>{
     const url = `${this.hostedServer}/specializations/${id}`;
-    return this.http.delete<void>(url)
+    return this.http.delete<void>(url, { headers: this.headers })
   }
 }
