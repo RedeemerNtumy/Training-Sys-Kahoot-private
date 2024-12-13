a33c1abc95342347dd0946c758a075d5af8d3bf6
Eric-Obeng <obengeric.3958@gmail.com>
2024-12-13 14:32:46 +0000

    feat: add custom headers for quiz data submission in setQuizData method

--- src/app/core/services/assessment/quiz-data.service.ts
+++ src/app/core/services/assessment/quiz-data.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, of, tap } from 'rxjs';
 import { AssessmentData } from '@core/models/assessment-form.interface';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
 
 @Injectable({
@@ -14,14 +14,24 @@ export class QuizDataService {
 
   constructor(private http: HttpClient) {}
 
-  setQuizData(data: FormData, postToBackend: boolean = false): Observable<AssessmentData | null> {
+  setQuizData(
+    data: FormData,
+    postToBackend: boolean = false
+  ): Observable<AssessmentData | null> {
+    const headers = new HttpHeaders({
+      'ngrok-skip-browser-warning': '74650',
+    });
     if (postToBackend) {
-      return this.http.post<AssessmentData>(`${environment.BaseUrl}/assessments/quiz`, data).pipe(
-        tap((response) => {
-          this.quizDataSubject.next(response);
-          sessionStorage.setItem('quizData', JSON.stringify(response));
+      return this.http
+        .post<AssessmentData>(`${environment.BaseUrl}/assessments/quiz`, data, {
+          headers,
         })
-      );
+        .pipe(
+          tap((response) => {
+            this.quizDataSubject.next(response);
+            sessionStorage.setItem('quizData', JSON.stringify(response));
+          })
+        );
     } else {
       this.quizDataSubject.next(data as unknown as AssessmentData);
       sessionStorage.setItem('quizData', JSON.stringify(data));
