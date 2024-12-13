f5f4f00f9f78370b47d5553f5331faba28f6b5c1
Eric-Obeng <obengeric.3958@gmail.com>
2024-12-13 11:44:10 +0000

    feat: enhance KahootHeaderComponent with question submission and improved modal handling

--- src/app/features/assessment-system/components/kahoot-header/kahoot-header.component.ts
+++ src/app/features/assessment-system/components/kahoot-header/kahoot-header.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, Input } from '@angular/core';
 import { GenerateByAiComponent } from '../../kahoot-page/generate-by-ai/generate-by-ai.component';
 import { RouterLink, RouterModule } from '@angular/router';
+import { Question } from '@core/models/kahoot-questions';
+import { KahootService } from '@core/services/assessment/kahoot/kahoot.service';
 
 @Component({
   selector: 'app-kahoot-header',
@@ -12,6 +14,12 @@ import { RouterLink, RouterModule } from '@angular/router';
 })
 export class KahootHeaderComponent {
   openAiModal: boolean = false;
+  @Input() questions: Question[] = [];
+
+  @Output() questionsAdded = new EventEmitter<Question[]>();
+  @Output() currentQuestionSubmitted = new EventEmitter<Question[]>();
+
+  constructor(private kahootService: KahootService) {}
 
   onOpenAiModal() {
     this.openAiModal = true;
@@ -19,5 +27,49 @@ export class KahootHeaderComponent {
 
   onCloseAiModal() {
     this.openAiModal = false;
+  }
+
+  handleQuestionsAdded(questions: Question[]) {
+    if (questions && Array.isArray(questions)) {
+      this.questionsAdded.emit(questions);
+      this.onCloseAiModal();
+    } else {
+      console.error('Invalid questions array:', questions);
+    }
+  }
+
+  onSubmit() {
+    this.currentQuestionSubmitted.emit(this.questions);
+
+    // Ensure questions array is not empty before emitting
+    if (this.questions.length > 0) {
+      const normalizedQuestions = this.questions.map((question) => {
+        const normalizeField = (field: any) => {
+          if (field.value && typeof field.value === 'object') {
+            return { name: field.name, value: field.value.value };
+          }
+          return field;
+        };
+
+        return {
+          ...question,
+          timeLimit: normalizeField(question.timeLimit),
+          points: normalizeField(question.points),
+        };
+      });
+
+      console.log('Normalized Questions:', normalizedQuestions);
+
+      this.kahootService.createKahootQuiz(normalizedQuestions).subscribe({
+        next: (response) => {
+          console.log('Quiz created successfully', response);
+        },
+        error: (error) => {
+          console.error('Error creating quiz', error);
+        },
+      });
+    } else {
+      console.error('No questions available to submit');
+    }
   }
 }
