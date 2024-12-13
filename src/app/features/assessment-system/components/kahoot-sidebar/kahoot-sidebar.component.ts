1a8c87b88b9fc5a06d4dc7d31cbc64b26e3dee92
Eric-Obeng <obengeric.3958@gmail.com>
2024-12-13 11:45:28 +0000

    feat: enhance KahootSidebarComponent with question selection, deletion, and improved data binding

--- src/app/features/assessment-system/components/kahoot-sidebar/kahoot-sidebar.component.ts
+++ src/app/features/assessment-system/components/kahoot-sidebar/kahoot-sidebar.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Question } from '@core/models/kahoot-questions';
 
 @Component({
   selector: 'app-kahoot-sidebar',
@@ -8,6 +9,51 @@ import { CommonModule } from '@angular/common';
   templateUrl: './kahoot-sidebar.component.html',
   styleUrl: './kahoot-sidebar.component.scss',
 })
-export class KahootSidebarComponent {
-  @Input() questions: any[] = [];
+export class KahootSidebarComponent implements OnInit, OnChanges {
+  @Input() questions: Question[] = [];
+  @Output() questionSelected = new EventEmitter<Question>();
+  @Output() questionsUpdated = new EventEmitter<Question[]>();
+  selectedQuestion: Question | null = null;
+
+  ngOnInit() {
+    this.setFirstQuestionActive();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['questions']) {
+      if (this.questions.length > 0 && (!this.selectedQuestion || changes['questions'].previousValue.length === 0)) {
+        this.setFirstQuestionActive();
+      }
+    }
+  }
+
+  private setFirstQuestionActive() {
+    if (this.questions.length > 0) {
+      this.selectedQuestion = this.questions[0];
+      this.questionSelected.emit(this.selectedQuestion);
+    }
+  }
+
+  onQuestionClick(question: Question) {
+    this.selectedQuestion = question;
+    this.questionSelected.emit(question);
+  }
+
+  deleteQuestion(question: Question) {
+    this.questions = this.questions.filter(q => q !== question);
+    this.questionsUpdated.emit(this.questions);
+    if (this.selectedQuestion === question) {
+      this.selectedQuestion = this.questions.length > 0 ? this.questions[0] : null;
+      if (this.selectedQuestion) {
+        this.questionSelected.emit(this.selectedQuestion);
+      }
+    }
+  }
+
+  addQuestion(question: Question) {
+    this.questions = [...this.questions, question];
+    if (this.questions.length === 1) {
+      this.setFirstQuestionActive();
+    }
+  }
 }
