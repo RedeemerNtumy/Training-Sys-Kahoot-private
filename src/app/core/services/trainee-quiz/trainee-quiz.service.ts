4583fb75354e3f793085c85c29b57cef5599e52c
Kojoel <nanaabruquah1s@gmail.com>
2024-12-13 11:35:51 +0000

    feat: get all assessments and populate template

--- src/app/core/services/trainee-quiz/trainee-quiz.service.ts
+++ src/app/core/services/trainee-quiz/trainee-quiz.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs';
+import { environment } from 'src/environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TraineeQuizService {
 
-  constructor() { }
+  quizUrl = `${environment.BaseUrl}/assignments/trainee`;
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      "ngrok-skip-browser-warning": "69420"
+    });
+  }
+
+  getAllAssignments(traineeEmail: string | undefined) {
+    const params = new HttpParams().set('traineeEmail', traineeEmail || '')
+    return this.http.get<any[]>(this.quizUrl, {
+      params,
+      headers: this.getHeaders() 
+    })
+  }
+
 }
