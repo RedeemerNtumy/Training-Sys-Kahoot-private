accf59c9bec5f78c0faf5fabe43a2be86aaab752
Eric-Obeng <obengeric.3958@gmail.com>
2024-12-13 11:44:27 +0000

    feat: refactor KahootQuestionsComponent to improve question and answer management with updated data binding

--- src/app/features/assessment-system/components/kahoot-questions/kahoot-questions.component.ts
+++ src/app/features/assessment-system/components/kahoot-questions/kahoot-questions.component.ts
@@ -1,33 +1,41 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Question } from '@core/models/kahoot-questions';
 
 @Component({
   selector: 'app-kahoot-questions',
   standalone: true,
   imports: [CommonModule, FormsModule],
   templateUrl: './kahoot-questions.component.html',
-  styleUrl: './kahoot-questions.component.scss'
+  styleUrls: ['./kahoot-questions.component.scss'],
 })
 export class KahootQuestionsComponent {
-  @Input() question: any = {
-    text: '',
-    answers: []
-  };
+  @Input() question!: Question;
   @Output() questionUpdate = new EventEmitter<any>();
 
+  trackByIndex(index: number, item: string): number {
+    return index;
+  }
+
   addAnswer() {
-    this.question.answers.push({
-      text: '',
-      correct: false
-    });
-    this.questionUpdate.emit(this.question);
+    if (this.question) {
+      this.question.options.push('');
+      this.questionUpdate.emit(this.question);
+    }
   }
 
   markAsCorrect(selectedAnswer: any) {
-    this.question.answers.forEach((answer: any) => {
-      answer.correct = answer === selectedAnswer;
-    });
+    if (this.question) {
+      this.question.correctAnswer = selectedAnswer;
+      this.questionUpdate.emit(this.question);
+    }
+  }
+
+  updateAnswer(index: number, event: Event, inputElement: HTMLInputElement) {
+    const target = event.target as HTMLInputElement;
+    this.question.options[index] = target.value;
+    setTimeout(() => inputElement.focus(), 0);
     this.questionUpdate.emit(this.question);
   }
 }
